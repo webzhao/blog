d661932e6d0057048b120be8d196fecaaa16300e
webzhao <webzhao@gmail.com>
2015-12-05 08:05:17 -0500

    update style

--- build/src/index.js
+++ build/src/index.js
@@ -120,7 +120,8 @@ async function parseMdFile(file) {
         title: meta.title,
         tags: meta.tags,
         content: marked(content),
-        summary: marked(content.substring(0, 300).replace(/[\r\n]+/g, ''))
+        summary: marked(content.substring(0, 300).replace(/[\r\n]+/g, '')),
+        meta
     }
 }
 
--- pages/about.md
+++ pages/about.md
@@ -1,3 +1,4 @@
 title: 关于我
+nav: about
 
 说明文字。
