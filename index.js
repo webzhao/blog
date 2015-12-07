617980ae770534184c56abec7c2492cb43047b6d
webzhao <webzhao@gmail.com>
2015-12-07 11:19:20 -0500

    add config file

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let command = process.argv[2];
 let build   = require('./build/');
 let http    = require('http');
 let fs      = require('fs');
+let crypto  = require('crypto');
+var exec    = require('child_process').exec;
+var config  = require('./config.js');
 
 //build
 if (command === 'build') {
@@ -22,12 +25,36 @@ if (command === 'write') {
 
 //start server to receive github post-commit hook
 let server = http.createServer(function(request, response) {
-    if (/^\/build/.test(request.url)) {
-        build();
-        response.end('ok');
-    } else {
-        response.end('error');
+    if (request.method != 'POST') {
+        response.statusCode = 405;
+        response.statusMessage = 'Method not allowed';
+        response.end('405 Method not allowed');
+        return;
     }
+    let body = '';
+    request.on('data', function(chunk) {
+        body += chunk.toString();
+    });
+    request.on('end', function() {
+        let secret_token = config.github_hook_key
+        let hmac = crypto.createHmac('sha1', secret_token);
+        hmac.update(body);
+        if (('sha1=' + hmac.digest('hex')) != request.headers['x-hub-signature']) {
+            response.end('fake request');
+            return;
+        }
+        process.nextTick(function() {
+            exec('git pull', function(error, stdout, stderr) {
+                if (error) {
+                    console.error(stderr);
+                    return;
+                }
+                console.log(stdout);
+                build();
+            });
+        });
+        response.end('ok');
+    });
 });
-server.listen(1111, '127.0.0.1');
+server.listen(config.github_hook_port);
 
